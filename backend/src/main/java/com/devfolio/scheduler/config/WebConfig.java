44e8af8cca121ab1c6aa8767dedc29e1d6f8e18d
saifawan129 <warriorahmed55@gmail.com>
2025-12-09 10:38:03 +0500

    CORS issue solved blocking the request

--- backend/src/main/java/com/devfolio/scheduler/config/WebConfig.java
+++ backend/src/main/java/com/devfolio/scheduler/config/WebConfig.java
@@ -10,7 +10,7 @@ public class WebConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
-                .allowedOrigins("http://localhost:3000", "http://127.0.0.1:3000") // Frontend URL
+                .allowedOriginPatterns("*") // Allow all for production debugging
                 .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
                 .allowedHeaders("*")
                 .allowCredentials(true);
